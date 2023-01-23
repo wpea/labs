d7f4950716ed9bffbbb20397ffabc90c78e9af84
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-01-23 16:02:30 +0100

    style:updated the project leads dropdown to include new hires and fixed errors

--- components/createProject.jsx
+++ components/createProject.jsx
@@ -190,7 +190,7 @@ export default function CreateProject({ toggle }) {
                           className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
                         >
                           <option defaultValue>Select</option>
-                          <option>Treasure-Prasie</option>
+                          <option>Treasure-Praise</option>
                           <option>Maryam Salaudeen</option>
                           <option>Kayode Adedeji</option>
                           <option>Alex</option>
