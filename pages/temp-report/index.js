3d18141c61d04ca6bdb240b6ec4ed7939af98c1a
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-02-09 16:30:45 +0100

    style: centered login page

--- pages/temp-report/index.js
+++ pages/temp-report/index.js
@@ -4,10 +4,10 @@ import { WPEALogo, RightArrow } from "../../assets/SVGs";
 const TempReport = () => {
   const router = useRouter();
   return (
-    <div className="mx-auto flex flex-col w-1/5  ">
+    <div className="mx-auto content-center min-h-screen justify-center   flex flex-col w-1/5  ">
       <WPEALogo />
-      <p className="text-left mb-5">Enter Acccess Code :</p>
-      <input type="text" className="rounded mb-5" />
+      <p className="text-left mt-5 mb-5">Enter Acccess Code :</p>
+      <input type="text" className="rounded mb-5 w-full" />
       <button
         className="border bg-[#2D7EC2] w-full rounded-md p-2  mx-auto"
         onClick={() => {
