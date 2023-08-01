6b33a91a48b13dd131555d48535950f7394d08aa
Alex <alex@wealthparadigm.org>
2023-08-01 16:00:39 +0100

    update to FM card page

--- components/asset-allocation/Card.jsx
+++ components/asset-allocation/Card.jsx
@@ -7,15 +7,11 @@ const Card = ({ src, href, name, description }) => {
       <Link
         passHref
         href={`asset-allocation/fundmanager/${href}`}
-        className="max-w-sm flex items-center justify-center  overflow-hidden  p-6 w-72 h-32 border  rounded-lg shadow bg-[#F4F5F6] dark:border-gray-700"
       >
-        <Image
-          alt="fund manager logo"
-          className="text-left mb-8 items-center justify-center "
-          src={src}
-          width={148}
-          height={148}
-        />
+        <div className="cursor-pointer border bg-gray-100 grid gap-5 grid-flow-col  auto-cols-max rounded-md place-items-center py-4 px-4 shadow-sm bg-center">
+          <div className="w-16 h-16 bg-center bg-contain bg-no-repeat bg-gray-200 rounded-full" style={{ backgroundImage: `url(${src})`}}></div>
+          <div className="capitalize">Fund manager name</div>
+          </div>
       </Link>
     </>
   );
